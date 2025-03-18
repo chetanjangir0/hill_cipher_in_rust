d7766ed70d1a95e7c047d2853d3c2635c0e7e8b9
Chetan <chetanrjj2002@gmail.com>
2025-03-18 14:18:19 +0000

    added empty key test

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,33 @@
 
-pub fn hill(text: &str, keyMatrix: &[&[i32]]) -> String{
+
+pub fn encode_hill(text: &str, keyMatrix: Vec<Vec<i32>>) -> Result<String, &'static str>{
+    let n = keyMatrix.len();
+    if n == 0 {
+        return Err("Error: key is empty");
+    }
+
+    if !keyMatrix.iter().all(|row| row.len() == n) {
+        return Err("Error: key matrix should be a square matrix");
+    }
+
+    Ok(String::from("ok"))
+
 
 }
 
+// pub fn decode_hill() {}
 
 
 
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty_key() {
+        assert_eq!(encode_hill("test", vec![]), Err("Error: key is empty"))
+    }
+
+}
